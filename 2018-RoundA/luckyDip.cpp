70cffc4d00e810ee38d3329325d614feda41b583
YeahHuang <534852491@qq.com>
2020-03-19 03:04:42 -0400

    Add files via upload

--- 2018-RoundA/luckyDip.cpp
+++ 2018-RoundA/luckyDip.cpp
@@ -16,6 +16,7 @@
 #include <vector>
 #include <complex>
 #include <algorithm>
+#include <iomanip>
 using namespace std;
 
 
@@ -60,16 +61,13 @@ Vi value 1..N
 
 12:21 吃了很多当安慰剂
 12:32 还是想不通 
-
-12:40 WA小的
-12:58 枚举还是WA
 */
 
 
 const int N = 2e4 + 10; // 这样更好 防止下面弄着弄着就少了个0
 int q,p1,p2,T, t, k, n;
 long long v[N];
-long double sum, avg, ans,sum1, sum2;
+double sum, avg, ans,sum1, sum2;
 bool flag;
 string s;
 vector<int> m[100015];
@@ -84,36 +82,26 @@ int main(){
             cin >> v[i];
             sum += v[i];
         }  
-        avg = sum / n;   
-        //avg = sum /= n; 一开始这里错误的写成了/=
-        ans = avg; 
+        avg = sum * 1.0 / n;    
         if (k == 0)
             ans = avg;
         else{
             p1 = 0;
             sum1 = 0.0;
-            sum2 = sum;
-            sort(v, v+n);
-            //debuga1(v, 0,n);
-            fto(i,0,n){
-                p1 = i+1;
-                sum2 -= v[i];
-                //debug(p1);
-                //debug(sum2);
-                ans = max(ans, p1*1.0/n * avg + (1-p1*1.0/n) * sum2/(n-p1));
-
-                //ans = max(ans, pow(p1*1.0/n, 2) * sum1 / p1 + (1 - pow(p1*1.0/n, 2)) * sum2/p2);
-            }
-
-               /* 
-                if (v[i] - avg < EPS){
+            sum2 = 0.0;
+            fto(i,0,n)
+                sum1 += (v[i]-avg<EPS)?avg:v[i];
+                /*if (v[i] - avg < EPS){
                     p1 += 1;
                     sum1 += v[i];
                 }else{
                     p2 += 1;
                     sum2 += v[i];
-                }*/       
+                }*/
+            ans = sum1 / n;
+            //ans = pow(p1*1.0/n, 2) * sum1 / p1 + (1 - pow(p1*1.0/n, 2)) * sum2/p2;
         }
-        cout << "Case #" << it + 1 << ": "<<ans<<endl;
+        printf("Case #%d: %.6lf\n",it+1,ans);
+        //cout << "Case #" << it + 1 << ": "<<setprecision(6)<<ans<<endl;
     }
 }
